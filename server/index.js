39c7567ff2a249d7e0bfc20d0ea04761df529c7f
Gabe Hargrave <gkhargrave@gmail.com>
2018-05-09 21:13:07 -0700

    Install Blipp

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 /* eslint-disable no-console */
 
 const Hapi = require('hapi');
+const Blipp = require('blipp');
 
 // Create a server with a host and port
 const server = Hapi.server({
@@ -8,24 +9,26 @@ const server = Hapi.server({
   port: 3001,
 });
 
-// Add the route
-server.route({
-  method: 'GET',
-  path: '/hello',
-  handler: (request, h) => (
-    { data: { message: 'hello world' } }
-  ),
-});
+(async() => {
+  await server.register(Blipp);
+  server.route({
+    method: 'GET',
+    path: '/hello',
+    handler: (request, h) => (
+      { data: { message: 'hello world' } }
+    ),
+  });
 
-// Start the server
-async function start() {
-  try {
-    await server.start();
-  } catch (err) {
-    console.log(err);
-    process.exit(1);
+  // Start the server
+  async function start() {
+    try {
+      await server.start();
+    } catch (err) {
+      console.log(err);
+      process.exit(1);
+    }
+    console.log('Server running at:', server.info.uri);
   }
-  console.log('Server running at:', server.info.uri);
-}
+  start();
+})();
 
-start();
