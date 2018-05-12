6e7ce4e5be4b2da490cd8f3d83a9c3c3c95b0aa3
Gabe Hargrave <gkhargrave@gmail.com>
2018-05-12 16:31:43 -0700

    Expand logging capabilities

--- utils/logger.js
+++ utils/logger.js
@@ -1,20 +1,30 @@
 /* eslint-disable no-console */
-module.exports = {
-  log(data) {
-    let lines = data;
-    if (!Array.isArray(lines)) {
-      lines = [lines];
-    }
-    lines.forEach(line => console.log(`${line}`));
-  },
+function log(data) {
+  let lines = data;
+  if (!Array.isArray(lines)) {
+    lines = [lines];
+  }
+  lines.forEach(line => console.log(`${line}`));
+}
 
-  verboseLog(_data) {
-    let lines = _data;
-    if (!Array.isArray(lines)) {
-      lines = [lines];
-    }
-    console.log('==============================================================');
-    lines.forEach(line => console.log(`=  ${line}`));
-    console.log('==============================================================');
-  },
+function verboseLog(data, devOnly = false) {
+  if (devOnly && process.env.NODE_ENV !== 'development') {
+    return;
+  }
+  const lines = Array.isArray(data) ? data : [data];
+  const out = [];
+  out.push('='.repeat(62));
+  lines.forEach(line => out.push(`= ${line}`));
+  out.push('='.repeat(62));
+  console.log(out.join('\n'));
+}
+
+function verboseLogDev(data) {
+  verboseLog(data, true);
+}
+
+module.exports = {
+  log,
+  verboseLog,
+  verboseLogDev,
 };
