f5ce89a801ae6cda99f4af11e70f2d24ca7ff505
Gabe Hargrave <gkhargrave@gmail.com>
2018-05-13 20:32:06 -0700

    Maximum shrinkifaction of apiWrapper

--- test/apiWrapper.js
+++ test/apiWrapper.js
@@ -1,16 +1,17 @@
 const axios = require('axios');
 
-const BASE = 'http://localhost:3001/api/v1';
+const BASE_URL = 'http://localhost:3001/api/v1';
 const DEFAULT_HEADERS = { Accept: 'application/json' };
 
 function buildReq(method, url, data = null, token = null) {
   const req = {
     method,
     headers: { ...DEFAULT_HEADERS },
-    url: `${BASE}${url}`,
+    url: `${BASE_URL}${url}`,
   };
-
+  // delete this to make sure the response is clean from the start
   delete req.headers.authorization;
+
   if (data) req.data = data;
   if (token) {
     req.headers.authorization = token;
@@ -19,69 +20,23 @@ function buildReq(method, url, data = null, token = null) {
   return req;
 }
 
+async function request(method, url, data = null, token = null) {
+  try {
+    const req = buildReq(method, url, data, token);
+    const res = await axios(req);
+    return { data: res.data.data || [] };
+  } catch (err) {
+    const error = err.response.data.data
+      ? err.response.data.data
+      : { error: err.response.data };
+    return { data: error };
+  }
+}
+
 module.exports = {
-  async get(url, token = null) {
-    try {
-      const req = buildReq('get', url, null, token);
-      const res = await axios(req);
-      return { data: res.data.data || [] };
-    } catch (err) {
-      const error = err.response.data.data
-        ? err.response.data.data
-        : { error: err.response.data };
-      return { data: error };
-    }
-  },
-
-  async post(url, payload, token = null) {
-    try {
-      const req = buildReq('post', url, payload, token);
-      const res = await axios(req);
-      return { data: res.data.data || [] };
-    } catch (err) {
-      const error = err.response.data.data
-        ? err.response.data.data
-        : { error: err.response.data };
-      return { data: error };
-    }
-  },
-
-  async put(url, payload, token = null) {
-    try {
-      const req = buildReq('put', url, payload, token);
-      const res = await axios(req);
-      return { data: res.data.data || [] };
-    } catch (err) {
-      const error = err.response.data.data
-        ? err.response.data.data
-        : { error: err.response.data };
-      return { data: error };
-    }
-  },
-
-  async patch(url, payload, token = null) {
-    try {
-      const req = buildReq('patch', url, payload, token);
-      const res = await axios(req);
-      return { data: res.data.data || [] };
-    } catch (err) {
-      const error = err.response.data.data
-        ? err.response.data.data
-        : { error: err.response.data };
-      return { data: error };
-    }
-  },
-
-  async del(url, token = null) {
-    try {
-      const req = buildReq('delete', url, null, token);
-      const res = await axios(req);
-      return { data: res.data.data || [] };
-    } catch (err) {
-      const error = err.response.data.data
-        ? err.response.data.data
-        : { error: err.response.data };
-      return { data: error };
-    }
-  },
+  get: async(url, token = null) => request('get', url, null, token),
+  post: async(url, payload, token = null) => request('post', url, payload, token),
+  put: async(url, payload, token) => request('put', url, payload, token),
+  patch: async(url, payload, token) => request('patch', url, payload, token),
+  del: async(url, token) => request('delete', url, null, token),
 };
