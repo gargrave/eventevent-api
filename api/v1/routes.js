a6955e8cceda7f40127257d7cad140365e59adad
Gabe Hargrave <gkhargrave@gmail.com>
2018-05-17 15:32:39 -0700

    Formatting

--- api/v1/routes.js
+++ api/v1/routes.js
@@ -2,17 +2,16 @@
 const fs = require('fs');
 
 // Dynamically builds HapiJS routes based on any files found with the sibling 'routes' directory
-module.exports =
-  () => {
-    const routes = `${__dirname}/routes`;
-    const dirs = fs
-      .readdirSync(routes)
-      .filter(file => fs.lstatSync(`${routes}/${file}`).isDirectory());
+module.exports = () => {
+  const routes = `${__dirname}/routes`;
+  const dirs = fs
+    .readdirSync(routes)
+    .filter(file => fs.lstatSync(`${routes}/${file}`).isDirectory());
 
-    return dirs.reduce((accum, val) => {
-      const dir = `${routes}/${val}`;
-      return accum.concat(
-        fs.readdirSync(dir).map(d => `${dir}/${d}`)
-      );
-    }, []);
-  };
+  return dirs.reduce((accum, val) => {
+    const dir = `${routes}/${val}`;
+    return accum.concat(
+      fs.readdirSync(dir).map(d => `${dir}/${d}`)
+    );
+  }, []);
+};
