cf506ea542e054113ba02c6d2792ba5f695c9378
Gabe Hargrave <gkhargrave@gmail.com>
2018-05-18 19:33:34 -0700

    Read CORS settings from env. vars

--- server/config/default.js
+++ server/config/default.js
@@ -1,8 +1,13 @@
+const cors = process.env.CORS_WHITELIST || '';
+
 const config = {
   server: {
     port: process.env.port || 3001,
     router: {
       stripTrailingSlash: true,
+    },
+    routes: {
+      cors: { origin: cors.split(',') },
     },
   },
   register: {
